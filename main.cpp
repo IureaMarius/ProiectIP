52ef1e9d7eefc4b2819fedb355850beb732d9696
Marius Iurea <marius.iurea@yahoo.com>
2019-12-06 22:39:00 +0200

    Finished working on the checkMoveValidity function, it now returns true if the move passed by argument is valid, it uses the auxiliary functions isAdjacentTo, checkPointBounds and sortMoveByValue.

--- main.cpp
+++ main.cpp
@@ -2,13 +2,25 @@
 #include <math.h>
 #include <stdlib.h>
 using namespace std;
-int GameBoard[4][4];
+/*
+PLAYER 1 IS RED AND PLAYER 2 IS BLUE,
+IN THE GAME BOARD MATRIX THE CELLS WITH THE BLUE L WILL BE MARKED BY 1
+AND THE CELLS WITH THE RED CELLS WILL BE MARKED BY -1
+THE NEUTRAL PIECES WILL BE MARKED BY 2
+THE EMPTY TILES WILL BE MARKED BY 0
+*/
 struct Point{
-int x;
-int y;
+    int x;
+    int y;
 };
-
+//GLOBAL VARIABLES
 Point selectedMove[4];
+int GameBoard[4][4]={2, 0, -1, -1,
+                     1, 0, 0, -1,
+                     1, 0, 0, -1,
+                     1, 1, 0, 2};
+int currentPlayer=-1;
+
 
 bool checkPointBounds(Point A)
 {
@@ -35,17 +47,21 @@ for(int i=0;i<4;i++)
 }
 bool isAdjacentTo(Point A,Point B)
 {
-    if(abs(A.x-B.x)==1||abs(A.y-B.y)==1)
+    if((A.x==B.x&&abs(A.y-B.y)==1)||(A.y==B.y&&abs(A.x-B.x)==1))
         return true;
     return false;
 }
 bool checkMoveValidity(Point Move[4])
 {
+    //CHECK IF THE POINTS DON'T GO OUTSIDE THE GAMEBOARD
+    for(int i=0;i<4;i++)
+        if(!checkPointBounds(Move[i]))
+            return false;
     //CHECK IF 3 ARE ON THE SAME LINE OR COLUMN
-    int MoveCounter[4];
+    int MoveCounter[4]={0};
     for(int i=0;i<4;i++)
     {
-        for(int j=i;j<4;j++)
+        for(int j=i+1;j<4;j++)
           {
 
             if(Move[i].x==Move[j].x)
@@ -62,16 +78,46 @@ bool checkMoveValidity(Point Move[4])
           }
 
     }
+    //IF THE CELLS ARE IN THE L POSITION THE VALUES IN THE SORTED MOVECOUNTER ARRAY WILL BE 1 2 2 3
+    sortMoveByValue(MoveCounter);
+    int shapeCheckArray[4]={1,2,2,3};
+    for(int i=0;i<4;i++)
+        if(MoveCounter[i]!=shapeCheckArray[i])
+            return false;
+    //CHECK IF THEY ARE ADJACENT
+    int adjacencyCounter=0;//
+    for(int i=0;i<4;i++)
+    {
+        for(int j=i;j<4;j++)
+        {
+            if(isAdjacentTo(Move[i],Move[j]))
+                adjacencyCounter++;
+        }
+    }
+    if(adjacencyCounter!=3)
+        return false;
+    //CHECK IF THE CELLS SELECTED ARE AVAILABLE
+    for(int i=0;i<4;i++)
+        if(GameBoard[Move[i].x][Move[i].y]==-currentPlayer)
+            return false;
 
-
-
-
-    //CHECK IF THEY ARE NEXT TO EACHOTHER
-    //CHECK IF THE LAST TILE IS IN ONE OF THE 4 VALID PLACES
+    return true;
 }
 
 int main()
 {
-
+    //TESTING VALUES
+    selectedMove[0].x=2;
+    selectedMove[0].y=1;
+    selectedMove[1].x=2;
+    selectedMove[1].y=0;
+    selectedMove[2].x=2;
+    selectedMove[2].y=2;
+    selectedMove[3].x=2;
+    selectedMove[3].y=3;
+    if(checkMoveValidity(selectedMove))
+        cout<<"VALID MOVE";
+    else cout<<"INVALID MOVE";
+    //TODO: CHECK MORE CORNER CASES AND MAKE SURE THE FUNCTION DOESN'T HAVE ANY BUGS
     return 0;
 }
